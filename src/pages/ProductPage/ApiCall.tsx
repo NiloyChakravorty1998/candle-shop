e4ffc0aa886f51a24878b174fb6923f08a537dcb
chakran <niloy.chakravorty@ppd.com>
2024-11-28 17:10:47 +0530

    swtiched to render

--- src/pages/ProductPage/ApiCall.tsx
+++ src/pages/ProductPage/ApiCall.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios, { AxiosResponse } from "axios";
 import { Product } from "../../components/Products/Types";
 import { useParams } from "react-router-dom";
+import { PRODUCTS_BASE_URL } from "../../constants/AppConstants";
 
 const useGetCurrentProduct = () => {
   const [data, setData] = useState<Product | null>(null);
@@ -14,7 +15,7 @@ const useGetCurrentProduct = () => {
     const fetchProduct = async () => {
       try {
         setIsLoading(true); 
-        const response: AxiosResponse<Product> = await axios.get(`http://localhost:3000/products/${id}`);
+        const response: AxiosResponse<Product> = await axios.get(`${PRODUCTS_BASE_URL}/products/${id}`);
         setData(response.data);
       } catch (err) {
         console.error('Error fetching product:', err);
